a5173447043b77d137dfc89b8a01d95c9a6bee48
Pablo Jean Rozario <pablo.jean.eel@gmail.com>
2024-03-31 23:50:52 -0300

    :dire: reading the registers. Remember, ADS131 uses SPI Mode 1

--- ADS131M0x.c
+++ ADS131M0x.c
@@ -11,7 +11,7 @@
  * Macros
  */
 
-#define _SYNC_RESET_ON		0
+#define _SYNC_ON		0
 #define _SYNC_OFF		1
 #define _CS_ON			0
 #define _CS_OFF			1
@@ -42,6 +42,10 @@ static void __sync_reset_pin(ads131_t *Ads131, uint8_t sync_reset){
 		Ads131->fxn.SYNCPin(sync_reset);
 }
 
+static __delay_ms(ads131_t *Ads131, uint32_t ms){
+	Ads131->fxn.DelayMs(ms);
+}
+
 static void __lock_spi(ads131_t *Ads131){
 	if (Ads131->fxn.Lock != NULL)
 		Ads131->fxn.Lock();
@@ -54,52 +58,55 @@ static void __unlock_spi(ads131_t *Ads131){
 
 /* Middle functions */
 
-ads131_err_e _read_regs(ads131_t *Ads131, uint32_t reg, uint16_t *val){
-	uint8_t frame[12];
-	uint8_t frameLenin8b;
-	uint32_t *a = frame;
-
-	memset(frame, 0, sizeof(frame));
-
-	frame;
-	memcpy(frame, &reg, 3);
+uint32_t _xfer_word(ads131_t *Ads131, uint32_t *TxWord){
+	uint32_t RxWord[4] = {0};
 
 	__lock_spi(Ads131);
 	__cs_pin(Ads131, _CS_ON);
-	__write_spi(Ads131, frame, sizeof(frame));
-	__cs_pin(Ads131, _CS_OFF);
-
-	memset(frame, 0, sizeof(frame));
-	__cs_pin(Ads131, _CS_ON);
-	__read_spi(Ads131, frame, 3);
+	__transfer_spi(Ads131, (uint8_t*)TxWord, (uint8_t*)RxWord, Ads131->WordSize*1);
 	__cs_pin(Ads131, _CS_OFF);
 	__unlock_spi(Ads131);
 
-	*val = (uint16_t)(*a >> 8);
+	return RxWord[0];
+}
+
+ads131_err_e _read_regs(ads131_t *Ads131, uint32_t reg, uint16_t *val){
+	uint32_t frame[4] = {0};
+	uint32_t rregCmd;
+	uint32_t status;
+
+	rregCmd = ADS131_CMD_RREG | ( (reg & 0x3F) << 7 );
+	frame[0] = ( (rregCmd >> 8) & 0xFF) | ( (rregCmd & 0xFF) << 8);
+
+	status = _xfer_word(Ads131, frame);
+
+	status = _xfer_word(Ads131, ADS131_CMD_NULL);
+
+	*val = (uint16_t)((status >> 8) & 0xFF) | ( (status & 0xFF) << 8);
 }
 
 ads131_err_e _write_regs(ads131_t *Ads131, uint32_t reg, uint16_t val){
-	uint8_t frame[12];
-	uint8_t frameLenin8b;
-	uint32_t *a = frame;
-
-	memset(frame, 0, sizeof(frame));
-
-	reg <<= 8;
-	memcpy(frame, &reg, 3);
-
-	__lock_spi(Ads131);
-	__cs_pin(Ads131, _CS_ON);
-	__write_spi(Ads131, frame, sizeof(frame));
-	__cs_pin(Ads131, _CS_OFF);
-
-	memset(frame, 0, sizeof(frame));
-	__cs_pin(Ads131, _CS_ON);
-	__read_spi(Ads131, frame, 3);
-	__cs_pin(Ads131, _CS_OFF);
-	__unlock_spi(Ads131);
-
-	*val = (uint16_t)(*a >> 8);
+//	uint8_t frame[12];
+//	uint8_t frameLenin8b;
+//	uint32_t *a = frame;
+//
+//	memset(frame, 0, sizeof(frame));
+//
+//	reg <<= 8;
+//	memcpy(frame, &reg, 3);
+//
+//	__lock_spi(Ads131);
+//	__cs_pin(Ads131, _CS_ON);
+//	__write_spi(Ads131, frame, sizeof(frame));
+//	__cs_pin(Ads131, _CS_OFF);
+//
+//	memset(frame, 0, sizeof(frame));
+//	__cs_pin(Ads131, _CS_ON);
+//	__read_spi(Ads131, frame, 3);
+//	__cs_pin(Ads131, _CS_OFF);
+//	__unlock_spi(Ads131);
+//
+//	*val = (uint16_t)(*a >> 8);
 }
 
 /*
@@ -107,14 +114,27 @@ ads131_err_e _write_regs(ads131_t *Ads131, uint32_t reg, uint16_t val){
  */
 
 ads131_err_e ads131_init(ads131_t *Ads131){
+	ads131_reg_status_t status;
+	ads131_reg_id_t id;
+
 	if (Ads131 == NULL){
 		return ADS131_UNKNOWN;
 	}
 	if (Ads131->fxn.CSPin == NULL ||
-		Ads131->fxn.SPITransfer == NULL){
-
+		Ads131->fxn.SPITransfer == NULL ||
+		Ads131->fxn.DelayMs == NULL){
+		return ADS131_UNKNOWN;
 	}
 
+	Ads131->WordSize = 3;
+
+	__sync_reset_pin(Ads131, _SYNC_ON);
+	__delay_ms(Ads131, 5);
+	__sync_reset_pin(Ads131, _SYNC_OFF);
+	__delay_ms(Ads131, 5);
+
+	_read_regs(Ads131, ADS131_REG_STATUS, &status._raw);
+	_read_regs(Ads131, ADS131_REG_ID, &id._raw);
 }
 
 ads131_err_e ads131_write_reg(ads131_t *Ads131, uint32_t reg, uint16_t val){
--- ADS131M0x.h
+++ ADS131M0x.h
@@ -13,7 +13,8 @@
  */
 
 #include <stdint.h>
-#include <cmsis_compiler.h>
+#include <stddef.h>
+#include <cmsis_gcc.h>
 
 /**
  * Macros
@@ -57,6 +58,12 @@
 #define ADS131_REG_REGMAP_CRC		0x3E
 #define ADS131_REG_RESERVED			0x3F
 
+#define ADS131_SET_FXN_LOCK(handle, Func)			handle.fxn.Lock = Func;
+#define ADS131_SET_FXN_UNLOCK(handle, Func)			handle.fxn.Unock = Func;
+#define ADS131_SET_FXN_CSPIN(handle, Func)			handle.fxn.CSPin = Func;
+#define ADS131_SET_FXN_SYNCPIN(handle, Func)		handle.fxn.SYNCPin = Func;
+#define ADS131_SET_FXN_DELAYMS(handle, Func)		handle.fxn.DelayMs = Func;
+#define ADS131_SET_FXN_SPITRANSFER(handle, Func) 	handle.fxn.SPITransfer = Func;
 
 /**
  * Enumerates
@@ -79,117 +86,150 @@ typedef enum{
 // Strucuts for the fields of each register of the
 // ADS131M0x, this is to make more easy the handleing of
 // the parameters
-typedef __PACKED_STRUCT{
-	uint8_t _r1 	: 8;
-	uint8_t CHANCNT : 4;
-	uint8_t _r2		: 4;
+typedef __PACKED_UNION{
+	__PACKED_STRUCT{
+		uint8_t _r1 	: 8;
+		uint8_t CHANCNT : 4;
+		uint8_t _r2		: 4;
+	};
+	uint16_t _raw;
 }ads131_reg_id_t;
 
-typedef __PACKED_STRUCT{
-	uint8_t DRDY0	: 1;
-	uint8_t DRDY1	: 1;
-	uint8_t _r1		: 6;
-	uint8_t WLENGTH : 2;
-	uint8_t RESET	: 1;
-	uint8_t CRC_TYPE: 1;
-	uint8_t CRC_ERR : 1;
-	uint8_t REG_MAP : 1;
-	uint8_t F_RESYNC: 1;
-	uint8_t LOCK	: 1;
+typedef __PACKED_UNION{
+	__PACKED_STRUCT{
+		uint8_t DRDY0	: 1;
+		uint8_t DRDY1	: 1;
+		uint8_t _r1		: 6;
+		uint8_t WLENGTH : 2;
+		uint8_t RESET	: 1;
+		uint8_t CRC_TYPE: 1;
+		uint8_t CRC_ERR : 1;
+		uint8_t REG_MAP : 1;
+		uint8_t F_RESYNC: 1;
+		uint8_t LOCK	: 1;
+	};
+	uint16_t _raw;
 }ads131_reg_status_t;
 
-typedef __PACKED_STRUCT{
-	uint8_t DRDY_FMT  : 1;
-	uint8_t DRDY_HiZ  : 1;
-	uint8_t DRDY_SEL  : 2;
-	uint8_t TIMEOUT   : 1;
-	uint8_t _r1		  : 3;
-	uint8_t WLENGTH   : 2;
-	uint8_t RESET     : 1;
-	uint8_t CRC_TYPE  : 1;
-	uint8_t RX_CRC_EN : 1;
-	uint8_t REG_CRC_EN: 1;
-	uin8t_t _r2		  : 1;
+typedef __PACKED_UNION{
+	__PACKED_STRUCT{
+		uint8_t DRDY_FMT  : 1;
+		uint8_t DRDY_HiZ  : 1;
+		uint8_t DRDY_SEL  : 2;
+		uint8_t TIMEOUT   : 1;
+		uint8_t _r1		  : 3;
+		uint8_t WLENGTH   : 2;
+		uint8_t RESET     : 1;
+		uint8_t CRC_TYPE  : 1;
+		uint8_t RX_CRC_EN : 1;
+		uint8_t REG_CRC_EN: 1;
+		uint8_t _r2		  : 1;
+	};
+	uint16_t _raw;
 }ads131_reg_mode_t;
 
-typedef __PACKED_STRUCT{
-	uint8_t PWR		: 2;
-	uint8_t OSR		: 2;
-	uint8_t TBM		: 1;
-	uint8_t _r1		: 2;
-	uint8_t CH0_EN	: 1;
-	uint8_t CH1_EN	: 1;
-	uint8_t _r2		: 6;
+typedef __PACKED_UNION{
+	__PACKED_STRUCT{
+		uint8_t POWER	: 2;
+		uint8_t OSR		: 2;
+		uint8_t TBM		: 1;
+		uint8_t _r1		: 2;
+		uint8_t CH0_EN	: 1;
+		uint8_t CH1_EN	: 1;
+		uint8_t _r2		: 6;
+	};
+	uint16_t _raw;
 }ads131_reg_clock_t;
 
-typedef __PACKED_STRUCT{
-	uint8_t PGAGAIN0 	: 3;
-	uint8_t _res1		: 1;
-	uint8_t PGAGAIN1	: 3;
-	uint8_t _res2		: 1;
-	uint8_t PGAGAIN2	: 3;
-	uint8_t _res3		: 1;
-	uint8_t PGAGAIN3	: 3;
-	uint8_t _res4		: 1;
+typedef __PACKED_UNION{
+	__PACKED_STRUCT{
+		uint8_t PGAGAIN0 	: 3;
+		uint8_t _res1		: 1;
+		uint8_t PGAGAIN1	: 3;
+		uint8_t _res2		: 1;
+		uint8_t PGAGAIN2	: 3;
+		uint8_t _res3		: 1;
+		uint8_t PGAGAIN3	: 3;
+		uint8_t _res4		: 1;
+	};
+	uint16_t _raw;
 }ads131_reg_gain1_t;
 
-typedef __PACKED_STRUCT{
-	uint8_t PGAGAIN4 	: 3;
-	uint8_t _res1		: 1;
-	uint8_t PGAGAIN5	: 3;
-	uint8_t _res2		: 1;
-	uint8_t PGAGAIN6	: 3;
-	uint8_t _res3		: 1;
-	uint8_t PGAGAIN7	: 3;
-	uint8_t _res4		: 1;
+typedef __PACKED_UNION{
+	__PACKED_STRUCT{
+		uint8_t PGAGAIN4 	: 3;
+		uint8_t _res1		: 1;
+		uint8_t PGAGAIN5	: 3;
+		uint8_t _res2		: 1;
+		uint8_t PGAGAIN6	: 3;
+		uint8_t _res3		: 1;
+		uint8_t PGAGAIN7	: 3;
+		uint8_t _res4		: 1;
+	};
+	uint16_t _raw;
 }ads131_reg_gain2_t;
 
-typedef __PACKED_STRUCT{
-	uint8_t CD_EN		: 1;
-	uint8_t CD_LEN		: 3;
-	uint8_t CD_NUM		: 3;
-	uint8_t CD_ALLCH	: 1;
-	uint8_t GC_EN		: 1;
-	uint8_t GC_DLY		: 4;
-	uint8_t _res1		: 3;
+typedef __PACKED_UNION{
+	__PACKED_STRUCT{
+		uint8_t CD_EN		: 1;
+		uint8_t CD_LEN		: 3;
+		uint8_t CD_NUM		: 3;
+		uint8_t CD_ALLCH	: 1;
+		uint8_t GC_EN		: 1;
+		uint8_t GC_DLY		: 4;
+		uint8_t _res1		: 3;
+	};
+	uint16_t _raw;
 }ads131_reg_cfg_t;
 
-typedef __PACKED_STRUCT{
+typedef __PACKED_UNION{
 	uint16_t CD_TH_MSB;
+}ads131_thrshld_msb_t;
+
+typedef __PACKED_UNION{
+	__PACKED_STRUCT{
+		uint8_t DCBLOCK		: 4;
+		uint8_t _res1		: 4;
+		uint8_t CD_TH_LSB	: 8;
+	};
+	uint16_t _raw;
 }ads131_thrshld_lsb_t;
 
-typedef __PACKED_STRUCT{
-	uint8_t DCBLOCK		: 4;
-	uint8_t _res1		: 4;
-	uint8_t CD_TH_LSB	: 8;
-}ads131_thrshld_lsb_t;
-
-typedef __PACKED_STRUCT{
-	uint8_t MUX			: 2;
-	uint8_t DCBLK_DIS0	: 1;
-	uint8_t _res1		: 3;
-	uint16_t PHASE		: 10;
+typedef __PACKED_UNION{
+	__PACKED_STRUCT{
+		uint8_t MUX			: 2;
+		uint8_t DCBLK_DIS0	: 1;
+		uint8_t _res1		: 3;
+		uint16_t PHASE		: 10;
+	};
+	uint16_t _raw;
 }ads131_chx_cfg_t;
 
-typedef __PACKED_STRUCT{
+typedef __PACKED_UNION{
 	uint16_t OCAL_MSB;
 }ads131_chx_ocal_msb_t;
 
-typedef __PACKED_STRUCT{
-	uint8_t _res1		: 8;
-	uint8_t OCAL_LSB	: 8;
+typedef __PACKED_UNION{
+	__PACKED_STRUCT{
+		uint8_t _res1		: 8;
+		uint8_t OCAL_LSB	: 8;
+	};
+	uint16_t _raw;
 }ads131_chx_ocal_lsb_t;
 
-typedef __PACKED_STRUCT{
+typedef __PACKED_UNION{
 	uint16_t GCAL_MSB;
 }ads131_chx_gcal_msb_t;
 
-typedef __PACKED_STRUCT{
-	uint8_t _res1		: 8;
-	uint8_t GCAL_LSB	: 8;
+typedef __PACKED_UNION{
+	__PACKED_STRUCT{
+		uint8_t _res1		: 8;
+		uint8_t GCAL_LSB	: 8;
+	};
+	uint16_t _raw;
 }ads131_chx_gcal_lsb_t;
 
-typedef __PACKED_STRUCT{
+typedef __PACKED_UNION{
 	uint16_t REG_CRC;
 }ads131_reg_regmap_crc_t;
 
@@ -200,6 +240,7 @@ typedef void (*Lock_f)(void);
 typedef void (*Unlock_f)(void);
 typedef void (*CSPin_f)(uint8_t Sig);
 typedef void (*SYNCPin_f)(uint8_t Sig);
+typedef void (*DelayMs_f)(uint32_t ms);
 typedef void (*SPITransfer_f)(uint8_t *Tx, uint8_t *Rx, uint32_t len);
 
 /* Main Handle of the ADS131M0x Driver */
@@ -209,10 +250,12 @@ typedef struct{
 		Unlock_f Unlock;
 		CSPin_f CSPin;
 		SYNCPin_f SYNCPin;
+		DelayMs_f DelayMs;
 		SPITransfer_f SPITransfer;
 	}fxn;
 	uint8_t Initialized;
 	uint8_t nChannels;
+	uint8_t WordSize;
 }ads131_t;
 
 typedef struct{
@@ -232,9 +275,9 @@ typedef struct{
 
 ads131_err_e ads131_init(ads131_t *Ads131);
 
-ads131_err_e ads131_write_reg(ads131_t *Ads131, uint8_t reg, uint8_t val);
+ads131_err_e ads131_write_reg(ads131_t *Ads131, uint32_t reg, uint16_t val);
 
-ads131_err_e ads131_read_reg(ads131_t *Ads131, uint8_t reg, uint8_t *val);
+ads131_err_e ads131_read_reg(ads131_t *Ads131, uint32_t reg, uint16_t *val);
 
 ads131_err_e ads131_read_one_channel(ads131_t *Ads131, uint32_t *val);
 
